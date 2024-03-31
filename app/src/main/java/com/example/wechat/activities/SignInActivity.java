802c09b5c44a8492bded1bffd41eeeec1f8ae8b5
4V1d03 <aguilarcastillo512@gmail.com>
2024-03-31 12:47:07 -0600

    Etapa 1 Completa
    Dos Activity creadas con su respectivo diseño

---
+++ app/src/main/java/com/example/wechat/activities/SignInActivity.java
@@ -0,0 +1,32 @@
+package com.example.wechat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.wechat.R;
+import com.example.wechat.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+
+    //como se habilito viewBinding para el proyecto, la clase binding para cada XML layout se va a generar automaticamente
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    //una instancia de una clase Binding contiene referencia directa a todos los views que tienen un ID que corresponde a un layout
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+
+
+
+}
