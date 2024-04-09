339c0bc45761309ccd19be3738ce0331d559e42f
Hector Palacios <alejandropalacios2821@gmail.com>
2024-04-08 23:09:44 -0600

    video 6
    -creación del menú de la lista de usuarios
    -NOTA: los usuarios quedan con demasiada separación, he verificado y no encuentro solución

--- app/src/main/java/com/example/wechat/activities/MainActivity.java
+++ app/src/main/java/com/example/wechat/activities/MainActivity.java
@@ -36,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners(){
+
         binding.imageSignOut.setOnClickListener(v -> singOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
     private void loadUserDetails(){
         binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
@@ -59,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
     private void singOut(){
