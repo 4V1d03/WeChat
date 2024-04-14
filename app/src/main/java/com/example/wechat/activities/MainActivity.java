2ef239a0aed4d6920311a280ccaf8e2ed1e8a217
mp_lemus <miltonjosuelemus@gmail.com>
2024-04-14 13:02:30 -0600

    Parte 11 Estado Online
    Creacion de Activity BaseActivity
    Modificacion en el ChatActivity
    Modificacion en MainActivity
    Modificacion en UsersActivity
    Creacion de ApiClient
    Creacion de ApiService
    Modificacion en Constants
    Modificacion en activity_chat
    Modificacion en strings

--- app/src/main/java/com/example/wechat/activities/MainActivity.java
+++ app/src/main/java/com/example/wechat/activities/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.wechat.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -32,7 +30,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ConversionListener {
+public class MainActivity extends BaseActivity implements ConversionListener {
 
     private ActivityMainBinding binding;
     private PreferenceManager preferenceManager ;
---
+++ app/src/main/java/com/example/wechat/network/ApiService.java
@@ -0,0 +1,19 @@
+package com.example.wechat.network;
+
+import androidx.annotation.StyleableRes;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    @POST("send")
+    Call<String> sendMessage(
+            @HeaderMap HashMap<String, String>headers,
+            @Body String messageBody
+    );
+}
