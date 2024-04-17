4a0b84e47d212e94e6f8d845ab9ca1cdb19de72b
fran780 <franciscofernandez365@yahoo.com>
2024-04-17 10:24:38 -0600

    Update ChatActivity.java

--- app/src/main/java/com/example/wechat/activities/ChatActivity.java
+++ app/src/main/java/com/example/wechat/activities/ChatActivity.java
@@ -115,6 +115,8 @@ public class ChatActivity extends BaseActivity{
                     body.put(Constants.REMOTE_MSG_DATA, data);
                     body.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
 
+                    sendNotification(body.toString());
+
                 } catch (Exception exception) {
                     showToast(exception.getMessage());
                 }
@@ -127,6 +129,39 @@ public class ChatActivity extends BaseActivity{
 
     private void showToast(String message){
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void sendNotification(String messageBody){
+        ApiClient.getClient().create(ApiService.class).sendMessage(
+                Constants.getRemoteMsgHeaders(),
+                messageBody
+        ).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if (response.isSuccessful()){
+                    try {
+                        if (response.body() != null){
+                            JSONObject responseJson = new JSONObject(response.body());
+                            JSONArray results = responseJson.getJSONArray("results");
+                            if (responseJson.getInt("failure") ==1 ){
+                                JSONObject error = (JSONObject) results.get(0);
+                               // showToast(error.getString("error"));
+                                return;
+                            }
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                  //  showToast("Notificacion enviada correctamente");
+                }else {
+                  //  showToast("Error: "+ response.code());
+                }
+            }
+            @Override
+            public void onFailure(@NonNull Call<String> call,@NonNull Throwable t) {
+              //  showToast(t.getMessage());
+            }
+        });
     }
 
     private void listenAvailabilityOfReceiver(){
